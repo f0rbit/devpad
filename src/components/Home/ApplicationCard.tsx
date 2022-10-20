4ce73744ce93a890a054f950769c4f67307aa08e
f0rbit <dev@forbit.dev>
2022-10-20 21:03:10 +1030

    change application card type to always include images

--- src/components/Home/ApplicationCard.tsx
+++ src/components/Home/ApplicationCard.tsx
@@ -1,5 +1,5 @@
 import Image, { StaticImageData } from "next/image";
-import HomeButton, { UnwrappedButton } from "@/components/Home/HomeButton";
+import { UnwrappedButton } from "@/components/Home/HomeButton";
 
 type ApplicationCardProps = {
     icon: string | JSX.Element;
@@ -8,7 +8,7 @@ type ApplicationCardProps = {
     version: string;
     reverse: boolean;
     link: string;
-    images?: StaticImageData[];
+    images: StaticImageData[];
 };
 
 const ApplicationCard = ({
@@ -26,42 +26,47 @@ const ApplicationCard = ({
                 <span className="absolute right-0 mr-5 -mt-1 whitespace-nowrap text-right font-mono text-pad-gray-200">
                     {version}
                 </span>
-                <div className="h-4 w-4 -ml-[10px] 2xl:-ml-[6px] rounded-full border-2 dark:border-pad-gray-900 bg-pad-purple-500"></div>
+                <div className="-ml-[10px] h-4 w-4 rounded-full border-2 bg-pad-purple-500 dark:border-pad-gray-900 2xl:-ml-[6px]"></div>
             </div>
             <div
-                style={{minHeight: "24rem"}}
+                style={{ minHeight: "24rem" }}
                 className={
-                    "relative md:border-none border-2 dark:md:bg-transparent light:bg-gray-50 dark:bg-pad-gray-800 dark:hover:border-pad-purple-500 dark:border-transparent flex h-max hover:scale-105 space-y-4 md:origin-left transition-all duration-500 rounded-3xl p-6 my-8 group flex-col " + (reverse ? "md:flex-row-reverse" : "md:flex-row")
+                    "light:bg-gray-50 group relative my-8 flex h-max flex-col space-y-4 rounded-3xl border-2 p-6 transition-all duration-500 hover:scale-105 dark:border-transparent dark:bg-pad-gray-800 dark:hover:border-pad-purple-500 md:origin-left md:border-none dark:md:bg-transparent " +
+                    (reverse ? "md:flex-row-reverse" : "md:flex-row")
                 }
             >
-                {images.length > 0 && <div style={{minWidth: "16rem"}} className="w-full md:w-1/2 drop-shadow-lg md:mx-8 flex justify-center items-center mt-8 ">
-                    {images?.map((image, index) => {
-                        return (
-                            <div key={index} className="w-full shadow-xl">
-                                <Image
-                                    src={image}
-                                    alt="project image"
-                                    width={1600}
-                                    height={900}
-                                    className="rounded-xl"
-                                ></Image>
-                            </div>
-                        );
-                    })}
-                </div>}
-                <div className="md:relative w-full md:w-1/2 md:bg-gray-100 dark:md:bg-pad-gray-800 md:border-2 dark:md:border-pad-gray-700 md:group-hover:border-pad-purple-500 md:shadow-lg p-4 rounded-3xl md:mx-8">
+                {images.length > 0 && (
+                    <div
+                        style={{ minWidth: "16rem" }}
+                        className="mt-8 flex w-full items-center justify-center drop-shadow-lg md:mx-8 md:w-1/2 "
+                    >
+                        {images?.map((image, index) => {
+                            return (
+                                <div key={index} className="w-full shadow-xl">
+                                    <Image
+                                        src={image}
+                                        alt="project image"
+                                        width={1600}
+                                        height={900}
+                                        className="rounded-xl"
+                                    ></Image>
+                                </div>
+                            );
+                        })}
+                    </div>
+                )}
+                <div className="w-full rounded-3xl p-4 md:relative md:mx-8 md:w-1/2 md:border-2 md:bg-gray-100 md:shadow-lg md:group-hover:border-pad-purple-500 dark:md:border-pad-gray-700 dark:md:bg-pad-gray-800">
                     <div className="flex flex-row items-center justify-start text-3xl font-bold drop-shadow-xl">
                         <div className="h-8 w-8 drop-shadow-md">{icon}</div>
-                        <div className="ml-4 dark:text-white text-gray-700">{title}</div>
+                        <div className="ml-4 text-gray-700 dark:text-white">
+                            {title}
+                        </div>
                     </div>
-                    <p className="mt-4 text-lg text-gray-500 dark:text-neutral-400 p-2">
+                    <p className="mt-4 p-2 text-lg text-gray-500 dark:text-neutral-400">
                         {description}
                     </p>
-                    <div className="w-full absolute bottom-4 right-0 md:opacity-0 group-hover:opacity-100 transition-opacity duration-500 flex justify-center items-center">
-                        <UnwrappedButton
-                            text={'Go!'}
-                            dest={link}
-                        />
+                    <div className="absolute bottom-4 right-0 flex w-full items-center justify-center transition-opacity duration-500 group-hover:opacity-100 md:opacity-0">
+                        <UnwrappedButton text={"Go!"} dest={link} />
                     </div>
                 </div>
             </div>
