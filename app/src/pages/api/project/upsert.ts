b5cfee0e4d10cf6eae741291a878ddf4d9226729
f0rbit <dev@forbit.dev>
2024-04-01 11:06:52 +1030

    implement `/project/upsert` endpoint

--- app/src/pages/api/project/upsert.ts
+++ app/src/pages/api/project/upsert.ts
@@ -1,20 +1,40 @@
 import type { APIContext } from "astro";
+import { upsert_user } from "../../../server/types";
+import { db } from "../../../../database/db";
+import { project } from "../../../../database/schema";
 
 export async function PATCH(context: APIContext) {
 	// first we need to validate that the user is logged in
 	if (!context.locals.user) {
-		return new Response(null, { status: 401	});
+		return new Response(null, { status: 401 });
 	}
-	
+
 	// extract the form contents from the input
-	const project = context.request.body;
+	const body = await context.request.json();
 
+	const parsed = upsert_user.safeParse(body);
+	if (!parsed.success) {
+		console.warn(parsed.error);
+		return new Response(parsed.error.message, { status: 400 });
+	}
+	const upsert_project = parsed.data;
 	// validate project contents using zod & return error if anything missing
-	console.log({ project });
+	console.log({ upsert_project });
 
-	// perform db upsert
+	// assert that the owner_id of upsert_project is same as logged in user
+	if (upsert_project.owner_id != context.locals.user.id) {
+		return new Response(null, { status: 401 });
+	}
 
-	// return the project data
-	
-	return new Response(JSON.stringify({ project_id: "test" }));
+	try {
+		// perform db upsert
+		const new_project = await db.insert(project).values(upsert_project).onConflictDoUpdate({ target: [project.owner_id, project.project_id], set: upsert_project }).returning();
+
+		if (new_project.length != 1) throw new Error(`Project upsert returned incorrect rows (${new_project.length}`);
+		// return the project data
+		return new Response(JSON.stringify(new_project[0]));
+	} catch (err) {
+		console.error(err);
+		return new Response(null, { status: 500 });
+	}
 }
