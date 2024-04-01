d3f86b7c9523a419f407f4f2defef634b561c3c9
f0rbit <dev@forbit.dev>
2024-04-01 12:36:22 +1030

    begin work on project sub-page

--- app/src/server/projects.ts
+++ app/src/server/projects.ts
@@ -1,7 +1,21 @@
 import { db } from "../../database/db";
 import { project } from "../../database/schema";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 
 export async function getUserProjects(user_id: string) {
 	return await db.select().from(project).where(eq(project.owner_id, user_id));
+}
+
+export async function getProject(user_id: string, project_id: string) {
+	if (!user_id) return { project: null, error: "No user ID" };
+	if (!project_id) return { project: null, error: "No project ID" };
+
+	try {
+		const search = await db.select().from(project).where(and(eq(project.owner_id, user_id), eq(project.project_id, project_id)));
+		if (!search || !search[0]) return { project: null, error: "Couldn't find project" };
+		return { project: search[0], error: null };
+	} catch (err) {
+		console.error(err);
+		return { project: null, error: "Internal Server Error" };
+	}
 }
--- app/src/server/types.ts
+++ app/src/server/types.ts
@@ -9,10 +9,10 @@ export const upsert_user = z.object({
 	repo_url: z.string().optional().nullable(),
 	icon_url: z.string().optional().nullable(),
 	status: z.union([ z.literal("DEVELOPMENT"), z.literal("PAUSED"), z.literal("RELEASED"), z.literal("LIVE"), z.literal("FINISHED"), z.literal("ABANDONED"), z.literal("STOPPED") ]),
-	deleted: z.boolean().optional().nullable(),
+	deleted: z.boolean().optional().nullable().default(false),
 	link_url: z.string().optional().nullable(),
 	link_text: z.string().optional().nullable(),
-	visiblity: z.union([ z.literal("PUBLIC"), z.literal("PRIVATE"), z.literal("HIDDEN"), z.literal("ARCHIVED"), z.literal("DRAFT"), z.literal("DELETED") ]),
+	visibility: z.union([ z.literal("PUBLIC"), z.literal("PRIVATE"), z.literal("HIDDEN"), z.literal("ARCHIVED"), z.literal("DRAFT"), z.literal("DELETED") ]),
 	current_version: z.string().optional()
 });
 
