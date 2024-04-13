1afdafd52c4f32fa790d36f7186b9690d2ee5a9b
f0rbit <dev@forbit.dev>
2024-04-14 08:39:13 +0930

    setup a test for streaming response back to UI

---
+++ app/src/pages/api/todo/test.ts
@@ -0,0 +1,22 @@
+
+
+// we want to testing streaming response to user
+
+import type { APIContext } from "astro";
+
+// have 5 steps, each 200ms apart
+export async function POST(context: APIContext) { 
+	const stream = new ReadableStream({
+		async start(controller) {
+			controller.enqueue("starting\n");
+			for (let i = 0; i < 5; i++) {
+				await new Promise((resolve) => setTimeout(resolve, 1000));
+				controller.enqueue(`step ${i + 1}\n`);
+			}
+			controller.close();
+		},
+	});
+	return new Response(stream, { status: 200 });
+}
+
+
