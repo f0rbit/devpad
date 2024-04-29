44ab16ec23f3777023c3590306e3dd033b4f5979
f0rbit <dev@forbit.dev>
2024-04-29 18:13:49 +0930

    update migrations
    should look into just using db push

--- app/database/drizzle/0000_famous_supernaut.sql
+++ app/database/drizzle/0000_famous_supernaut.sql
@@ -16,6 +16,53 @@ CREATE TABLE `api_key` (
 	FOREIGN KEY (`owner_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
+CREATE TABLE `checklist` (
+	`id` text PRIMARY KEY NOT NULL,
+	`task_id` text NOT NULL,
+	`name` text NOT NULL,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`updated_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`deleted` integer,
+	FOREIGN KEY (`task_id`) REFERENCES `task`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `checklist_item` (
+	`id` text PRIMARY KEY NOT NULL,
+	`checklist_id` text NOT NULL,
+	`parent_id` text,
+	`name` text NOT NULL,
+	`checked` integer DEFAULT false NOT NULL,
+	FOREIGN KEY (`checklist_id`) REFERENCES `checklist`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `goal` (
+	`id` text PRIMARY KEY NOT NULL,
+	`milestone_id` text NOT NULL,
+	`name` text NOT NULL,
+	`description` text,
+	`target_time` text,
+	`deleted` integer,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`updated_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`finished_at` text,
+	FOREIGN KEY (`milestone_id`) REFERENCES `milestone`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `milestone` (
+	`id` text PRIMARY KEY NOT NULL,
+	`project_id` text NOT NULL,
+	`name` text NOT NULL,
+	`description` text,
+	`target_time` text,
+	`deleted` integer,
+	`target_version` text,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`updated_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`finished_at` text,
+	`after_id` text,
+	FOREIGN KEY (`project_id`) REFERENCES `project`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
 CREATE TABLE `project` (
 	`id` text PRIMARY KEY NOT NULL,
 	`project_id` text NOT NULL,
@@ -31,7 +78,8 @@ CREATE TABLE `project` (
 	`link_url` text,
 	`link_text` text,
 	`visibility` text,
-	`current_version` text
+	`current_version` text,
+	`config_json` text
 );
 --> statement-breakpoint
 CREATE TABLE `project_goal` (
@@ -56,40 +104,43 @@ CREATE TABLE `session` (
 	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
+CREATE TABLE `tag` (
+	`id` text PRIMARY KEY NOT NULL,
+	`owner_id` text NOT NULL,
+	`title` text NOT NULL,
+	`color` text,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`updated_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`deleted` integer,
+	FOREIGN KEY (`owner_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
 CREATE TABLE `task` (
 	`id` text NOT NULL,
 	`title` text NOT NULL,
 	`progress` text,
 	`visibility` text,
-	`parent_id` text,
 	`goal_id` text,
+	`description` text,
+	`start_time` text,
+	`end_time` text,
+	`summary` text,
+	`priority` text,
 	`created_at` text DEFAULT (CURRENT_TIMESTAMP),
 	`updated_at` text DEFAULT (CURRENT_TIMESTAMP),
 	FOREIGN KEY (`id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action,
-	FOREIGN KEY (`parent_id`) REFERENCES `task`(`id`) ON UPDATE no action ON DELETE no action
-);
---> statement-breakpoint
-CREATE TABLE `task_module` (
-	`task_id` text NOT NULL,
-	`type` text NOT NULL,
-	`data` text DEFAULT '{}' NOT NULL,
-	`updated_at` text DEFAULT (CURRENT_TIMESTAMP),
-	FOREIGN KEY (`task_id`) REFERENCES `task`(`id`) ON UPDATE no action ON DELETE no action
+	FOREIGN KEY (`goal_id`) REFERENCES `goal`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
 CREATE TABLE `task_tag` (
-	`id` text PRIMARY KEY NOT NULL,
-	`owner_id` text NOT NULL,
-	`title` text NOT NULL,
-	`colour` text DEFAULT '#000000' NOT NULL,
-	FOREIGN KEY (`owner_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE no action
-);
---> statement-breakpoint
-CREATE TABLE `task_to_tags` (
 	`task_id` text NOT NULL,
 	`tag_id` text NOT NULL,
+	`primary` integer DEFAULT false NOT NULL,
+	`created_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	`updated_at` text DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+	PRIMARY KEY(`tag_id`, `task_id`),
 	FOREIGN KEY (`task_id`) REFERENCES `task`(`id`) ON UPDATE no action ON DELETE no action,
-	FOREIGN KEY (`tag_id`) REFERENCES `task_tag`(`id`) ON UPDATE no action ON DELETE no action
+	FOREIGN KEY (`tag_id`) REFERENCES `tag`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
 CREATE TABLE `todo_updates` (
@@ -124,6 +175,4 @@ CREATE TABLE `user` (
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `project_owner_id_project_id_unique` ON `project` (`owner_id`,`project_id`);--> statement-breakpoint
-CREATE UNIQUE INDEX `task_module_task_id_type_unique` ON `task_module` (`task_id`,`type`);--> statement-breakpoint
-CREATE UNIQUE INDEX `task_to_tags_task_id_tag_id_unique` ON `task_to_tags` (`task_id`,`tag_id`);--> statement-breakpoint
 CREATE UNIQUE INDEX `user_email_unique` ON `user` (`email`);
