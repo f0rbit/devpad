8cf1009a64ef97a886bfa32250c1c692492b2fb4
f0rbit <dev@forbit.dev>
2024-10-27 19:16:44 +1030

    pkg: add solidjs

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,18 +1,20 @@
 import { defineConfig } from 'astro/config';
-
 import node from "@astrojs/node";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	adapter: node({
-		mode: "standalone"
-	}),
-	vite: {
-		build: {
-			rollupOptions: {
-				external: ["bun", "bun:sqlite"],
-			}
-		}
-	}
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ["bun", "bun:sqlite"]
+      }
+    }
+  },
+  integrations: [solidJs()]
 });
