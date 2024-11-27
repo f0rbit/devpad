91483cf3b670f10870f970fe4336f9d8ff090cda
f0rbit <dev@forbit.dev>
2024-11-28 10:16:52 +1030

    feat: set exisitng `PENDING` updates to `IGNORED`

--- app/src/pages/api/project/scan.ts
+++ app/src/pages/api/project/scan.ts
@@ -95,7 +95,7 @@ async function* scan_repo(repo_url: string, access_token: string, folder_id: str
 	yield "decompressing repo\n";
 	child_process.execSync(`unzip ${repo_path} -d ${unzipped_path}`);
 	const config_path = "../todo-config.json"; // TODO: grab this from user config from 1. project config, 2. user config, 3. default config
-	
+
 	// the unzipped folder will have a folder inside it with the repo contents, we need that pathname for the parsing task
 	const files = await readdir(unzipped_path);
 	const folder_path = `${unzipped_path}/${files[0]}`;
@@ -104,7 +104,7 @@ async function* scan_repo(repo_url: string, access_token: string, folder_id: str
 	// generate the todo-tracker parse
 	yield "scanning repo\n";
 	child_process.execSync(`../todo-tracker parse ${folder_path} ${config_path} > ${unzipped_path}/new-output.json`);
-	
+
 	yield "saving output\n"
 	// for now, lets return response of the new-output.json file
 	const output_file = await (Bun.file(`${unzipped_path}/new-output.json`).text());
@@ -127,7 +127,7 @@ async function* scan_repo(repo_url: string, access_token: string, folder_id: str
 	yield "finding existing scan\n";
 	const new_id = new_tracker[0].id;
 	const old_id = await db.select().from(tracker_result).where(and(eq(tracker_result.project_id, project_id), eq(tracker_result.accepted, true))).orderBy(desc(tracker_result.created_at)).limit(1);
-	
+
 	var old_data = [] as any[];
 	if (old_id.length == 1 && old_id[0].data) {
 		// fetch all the codebase tasks from the old_id
@@ -161,8 +161,16 @@ async function* scan_repo(repo_url: string, access_token: string, folder_id: str
 	yield "reading diff\n";
 	const diff = await Bun.file(unzipped_path + "/diff-output.json").text();
 
-	console.log("inserting diff", diff);
-
+	yield "ignoring old updates\n";
+	// update any old todo_updates that had status == "PENDING" to status == "IGNORED"
+	try {
+		await db.update(todo_updates).set({ status: "IGNORED" }).where(and(eq(todo_updates.project_id, project_id), eq(todo_updates.status, "PENDING")));
+	} catch (e) {
+		console.error(e);
+		yield "error ignoring old updates\n";
+		return;
+	}
+	
 	yield "saving update\n";
 	await db.insert(todo_updates).values({
 		project_id: project_id,
