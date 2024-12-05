205c15be53a9a22c29f4ccb078c34a2b84906989
f0rbit <dev@forbit.dev>
2024-12-05 21:16:59 +1030

    refactor: replace `import.meta.env` with `Bun.env`

--- app/src/server/github.ts
+++ app/src/server/github.ts
@@ -1,10 +1,7 @@
 import { GitHub } from "arctic";
 
-console.log("meta", import.meta.env);
-console.log("process.env", process.env);
-console.log("bun.env", Bun.env);
 
 export const github = new GitHub(
-	import.meta.env.GITHUB_CLIENT_ID!,
-	import.meta.env.GITHUB_CLIENT_SECRET!
+	Bun.env.GITHUB_CLIENT_ID!,
+	Bun.env.GITHUB_CLIENT_SECRET!
 );
