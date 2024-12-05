6ce22fb43907ca3f872a63c0132b274a3c2d888d
f0rbit <dev@forbit.dev>
2024-12-05 20:49:35 +1030

    debug: add logging to chceck env variables

--- app/src/server/github.ts
+++ app/src/server/github.ts
@@ -1,5 +1,7 @@
 import { GitHub } from "arctic";
 
+console.log({ GITHUB_CLIENT_ID: import.meta.env.GITHUB_CLIENT_ID });
+
 export const github = new GitHub(
 	import.meta.env.GITHUB_CLIENT_ID!,
 	import.meta.env.GITHUB_CLIENT_SECRET!
