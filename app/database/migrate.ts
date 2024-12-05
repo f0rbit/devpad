16f102056dc571c47a539a1b9293cd87925d41e3
f0rbit <dev@forbit.dev>
2024-12-05 21:20:04 +1030

    fix: change migration DATABASE_FILE to point to `Bun.env`

--- app/database/migrate.ts
+++ app/database/migrate.ts
@@ -2,6 +2,6 @@ import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { Database } from "bun:sqlite";
 
-const sqlite = new Database(import.meta.env.DATABASE_FILE);
+const sqlite = new Database(Bun.env.DATABASE_FILE);
 const db = drizzle(sqlite);
 migrate(db, { migrationsFolder: "./database/drizzle" });
