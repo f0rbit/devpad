b83200689da0c7f90e1687dce36f569eeaf1c699
f0rbit <dev@forbit.dev>
2024-12-05 21:10:43 +1030

    debug: log out all the environment variables on login request

--- app/src/server/github.ts
+++ app/src/server/github.ts
@@ -1,6 +1,8 @@
 import { GitHub } from "arctic";
 
-console.log({ GITHUB_CLIENT_ID: import.meta.env.GITHUB_CLIENT_ID });
+console.log("meta", import.meta.env);
+console.log("process.env", process.env);
+console.log("bun.env", Bun.env);
 
 export const github = new GitHub(
 	import.meta.env.GITHUB_CLIENT_ID!,
