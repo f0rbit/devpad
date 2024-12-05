d12203e9217ada806d46730a51ac73f0380467af
f0rbit <dev@forbit.dev>
2024-12-05 22:00:54 +1030

    debug: remove auth debug logging

--- app/src/pages/api/auth/callback.ts
+++ app/src/pages/api/auth/callback.ts
@@ -8,7 +8,6 @@ import { eq } from "drizzle-orm";
 import { user } from "../../../../database/schema";
 
 export async function GET(context: APIContext): Promise<Response> {
-	console.log(0);
 	const code = context.url.searchParams.get("code");
 	const state = context.url.searchParams.get("state");
 	const storedState = context.cookies.get("github_oauth_state")?.value ?? null;
@@ -17,43 +16,31 @@ export async function GET(context: APIContext): Promise<Response> {
 	}
 
 	try {
-		console.log(1);
 		const tokens = await github.validateAuthorizationCode(code);
-		console.log(2)
 		const githubUserResponse = await fetch("https://api.github.com/user", {
 			headers: {
 				Authorization: `Bearer ${tokens.accessToken}`
 			}
 		});
-		console.log(3)
 		const githubUser: GitHubUser = await githubUserResponse.json();
-		console.log(4)
 
 		const existingUser = await db.select().from(user).where(eq(user.github_id, githubUser.id));
-		console.log(5)
 
 		// Replace this with your own DB client
 
 		if (existingUser && existingUser[0]) {
-		console.log(6)
 		const session = await lucia.createSession(existingUser[0].id, { access_token: tokens.accessToken });
 			const sessionCookie = lucia.createSessionCookie(session.id);
-		console.log(7)
 		context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
 			return context.redirect("/");
 		}
-		console.log(8)
 
 		const new_user = await db.insert(user).values({ github_id: githubUser.id, name: githubUser.login }).returning({ user_id: user.id });
-		console.log(9)
 
 		const session = await lucia.createSession(new_user[0].user_id, { access_token: tokens.accessToken });
-		console.log(10)
 		const sessionCookie = lucia.createSessionCookie(session.id);
 		context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-		console.log(11)
 
-		console.log("done");
 		return context.redirect("/");
 	} catch (e) {
 		console.error(e);
