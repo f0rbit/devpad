87b7d510d7f3308df96f62ccf6822500d661e92f
f0rbit <dev@forbit.dev>
2024-12-05 21:01:35 +1030

    debug: log out all environment variables

--- deployment/index.ts
+++ deployment/index.ts
@@ -3,8 +3,12 @@ import { handler as ssrHandler } from '../app/dist/server/entry.mjs';
 
 const app = express();
 
+console.log("process.env", process.env);
+console.log("bun.env", Bun.env);
+
 const base = '/';
 app.use(base, express.static('../app/dist/client/'));
 app.use(ssrHandler);
 
-app.listen(8080);
+console.log("started server on port", process.env.PORT);
+app.listen(process.env.PORT);
