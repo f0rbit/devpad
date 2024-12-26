c473bae0ea7ae41d74c7228342d5d766e09064d7
f0rbit <dev@forbit.dev>
2024-12-27 09:42:10 +1030

    feat: add function to get the branches & latest commit for a repo

--- app/src/server/github.ts
+++ app/src/server/github.ts
@@ -2,8 +2,8 @@ import { GitHub } from "arctic";
 
 
 export const github = new GitHub(
-	Bun.env.GITHUB_CLIENT_ID!,
-	Bun.env.GITHUB_CLIENT_SECRET!
+  Bun.env.GITHUB_CLIENT_ID!,
+  Bun.env.GITHUB_CLIENT_SECRET!
 );
 
 
@@ -11,3 +11,38 @@ export async function getRepo(owner: string, repo: string, access_token: string)
   return await fetch(`https://api.github.com/repos/${owner}/${repo}/zipball`, { headers: { "Accept": "application/vnd.github+json", "Authorization": `Bearer ${access_token}`, "X-GitHub-Api-Version": "2022-11-28" } });
 }
 
+
+/**
+ * fetch the branches & attach the latest commit to each branch
+ * @todo use octokit or use zod to validate types
+ */
+export async function getBranches(owner: string, repo: string, access_token: string) {
+  const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/branches`, { headers: { "Accept": "application/vnd.github+json", "Authorization": `Bearer ${access_token}`, "X-GitHub-Api-Version": "2022-11-28" } });
+  if (!response.ok) {
+    throw new Error("error fetching branches");
+  }
+  const commits = new Set<string>(); // store commits to fetch the details after
+  const branches = await response.json();
+  for (const branch of branches) {
+    commits.add(branch.commit.sha);
+  }
+  // fetch the commit details for each commit
+  const commit_details = await Promise.all(Array.from(commits).map(async (commit) => {
+    const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/commits/${commit}`, { headers: { "Accept": "application/vnd.github+json", "Authorization": `Bearer ${access_token}`, "X-GitHub-Api-Version": "2022-11-28" } });
+    if (!response.ok) {
+      throw new Error("error fetching commit details");
+    }
+    return (await response.json() as any).commit;
+  }));
+  // merge the commit details into the branches
+  branches.forEach((branch: any, index: any) => {
+    branch.commit = commit_details[index];
+  });
+
+  // sort branches by date
+  branches.sort((a: any, b: any) => {
+    return new Date(b.commit.committer.date).getTime() - new Date(a.commit.committer.date).getTime();
+  });
+
+  return branches;
+}
