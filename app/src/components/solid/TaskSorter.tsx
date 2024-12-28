9412a12fbda20f59344a06f22c219f2a319598ce
f0rbit <dev@forbit.dev>
2024-12-28 14:00:13 +1030

    feat: search for tasks

--- app/src/components/solid/TaskSorter.tsx
+++ app/src/components/solid/TaskSorter.tsx
@@ -1,7 +1,9 @@
-import { createEffect, createSignal } from "solid-js";
+import { For, createEffect, createSignal } from "solid-js";
 import type { Task as TaskType } from "../../server/tasks";
 import { TaskCard } from "./TaskCard";
 import type { Project } from "../../server/projects";
+import Search from "lucide-solid/icons/search";
+import ArrowDownWideNarrow from "lucide-solid/icons/sort-desc";
 
 const options = ["recent", "priority", "progress"] as const;
 
@@ -12,14 +14,23 @@ export type SortOption = (typeof options)[number];
 export function TaskSorter({ tasks, defaultOption, project_map, from }: { tasks: TaskType[]; defaultOption: SortOption; project_map: Record<string, Project>; from: string }) {
   const [selectedOption, setSelectedOption] = createSignal<SortOption>(defaultOption);
   const [sortedTasks, setSortedTasks] = createSignal<TaskType[]>([]);
+  const [search, setSearch] = createSignal("");
+
 
   // sort tasks based on selected option
   createEffect(() => {
     // filter out 'archived' and 'deleted' tasks
-    const filtered = tasks.filter((task) => {
+    let filtered = tasks.filter((task) => {
       if (task.task == null) return false;
       return task.task.visibility !== "ARCHIVED" && task.task.visibility !== "DELETED" && task.task.visibility != "HIDDEN";
     });
+
+    const search_term = search();
+    if (search_term.length > 0) {
+      filtered = filtered.filter((task) => {
+        return task.task.title.toLowerCase().includes(search_term.toLowerCase());
+      });
+    }
 
     const sorted = filtered.toSorted((a, b) => {
       if (a == null && b == null) return 0;
@@ -47,24 +58,31 @@ export function TaskSorter({ tasks, defaultOption, project_map, from }: { tasks:
   });
 
   return (
-    <div>
-      <select value={selectedOption()} onChange={(e) => setSelectedOption(e.target.value as SortOption)}>
-        {options.map((option) => (
-          <option value={option} selected={option === selectedOption()}>
-            {option}
-          </option>
-        ))}
-      </select>
+    <div class="flex-col" >
+      <div class="flex-row">
+        <Search />
+        <input type="text" placeholder="Search" value={search()} onInput={(e) => setSearch(e.target.value)} />
+        <ArrowDownWideNarrow />
+        <select value={selectedOption()} onChange={(e) => setSelectedOption(e.target.value as SortOption)}>
+          {options.map((option) => (
+            <option value={option} selected={option === selectedOption()}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
       <ul class="flex-col" style={{ gap: "9px" }}>
-        {sortedTasks().map((task) => {
-          const project = project_map[task.task.project_id!];
-          if (project == null) return null;
-          return (
-            <li>
-              <TaskCard task={task} project={project} from={from} />
-            </li>
-          );
-        })}
+        <For each={sortedTasks()}>
+          {(task) => {
+            const project = project_map[task.task.project_id!];
+            if (project == null) return null;
+            return (
+              <li>
+                <TaskCard task={task} project={project} from={from} />
+              </li>
+            );
+          }}
+        </For> 
       </ul>
     </div>
   );
