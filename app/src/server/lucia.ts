7c8d172939821da021141a54f4fb86e5b6b7dd66
f0rbit <dev@forbit.dev>
2024-12-31 09:27:54 +1030

    feat: add 'task_view' to context.locals.user

--- app/src/server/lucia.ts
+++ app/src/server/lucia.ts
@@ -2,38 +2,41 @@ import { DrizzleSQLiteAdapter } from "@lucia-auth/adapter-drizzle";
 import { Lucia } from "lucia";
 import { db } from "../../database/db";
 import { session, user } from "../../database/schema";
+import type { TaskView } from "./types";
 
 const adapter = new DrizzleSQLiteAdapter(db, session, user);
 
 export const lucia = new Lucia(adapter, {
-	sessionCookie: {
-		attributes: {
-			secure: import.meta.env.MODE == "production"
-		}
-	},
-	getUserAttributes: (attributes) => {
-		return {
-			// attributes has the type of DatabaseUserAttributes
-			github_id: attributes.github_id,
-			name: attributes.name
-		};
-	},
-	getSessionAttributes: (attributes) => {
-		return {
-			access_token: attributes.access_token
-		}
-	}
+  sessionCookie: {
+    attributes: {
+      secure: import.meta.env.MODE == "production"
+    }
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      // attributes has the type of DatabaseUserAttributes
+      github_id: attributes.github_id,
+      name: attributes.name,
+      task_view: attributes.task_view
+    };
+  },
+  getSessionAttributes: (attributes) => {
+    return {
+      access_token: attributes.access_token
+    }
+  }
 });
 
 declare module "lucia" {
-	interface Register {
-		Lucia: typeof lucia;
-		DatabaseUserAttributes: DatabaseUserAttributes;
-		DatabaseSessionAttributes: { access_token: string };
-	}
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+    DatabaseSessionAttributes: { access_token: string };
+  }
 }
 
 interface DatabaseUserAttributes {
-	github_id: number;
-	name: string;
+  github_id: number;
+  name: string;
+  task_view: TaskView;
 }
