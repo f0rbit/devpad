390f3e956bb5064052d4d1a310188236b279fc03
f0rbit <dev@forbit.dev>
2024-12-31 09:28:28 +1030

    feat: add endpoint for updating 'task_view'

--- app/src/server/types.ts
+++ app/src/server/types.ts
@@ -3,21 +3,21 @@ import type { todo_updates, tracker_result } from "../../database/schema";
 import type { Task } from "./tasks";
 
 export const upsert_project = z.object({
-	id: z.string().optional().nullable(),
-	project_id: z.string(),
-	owner_id: z.string(),
-	name: z.string(),
-	description: z.string(),
-	specification: z.string().optional().nullable(),
-	repo_url: z.string().optional().nullable(),
-	repo_id: z.number().optional().nullable(),
-	icon_url: z.string().optional().nullable(),
-	status: z.union([z.literal("DEVELOPMENT"), z.literal("PAUSED"), z.literal("RELEASED"), z.literal("LIVE"), z.literal("FINISHED"), z.literal("ABANDONED"), z.literal("STOPPED")]),
-	deleted: z.boolean().optional().nullable().default(false),
-	link_url: z.string().optional().nullable(),
-	link_text: z.string().optional().nullable(),
-	visibility: z.union([z.literal("PUBLIC"), z.literal("PRIVATE"), z.literal("HIDDEN"), z.literal("ARCHIVED"), z.literal("DRAFT"), z.literal("DELETED")]),
-	current_version: z.string().optional()
+  id: z.string().optional().nullable(),
+  project_id: z.string(),
+  owner_id: z.string(),
+  name: z.string(),
+  description: z.string(),
+  specification: z.string().optional().nullable(),
+  repo_url: z.string().optional().nullable(),
+  repo_id: z.number().optional().nullable(),
+  icon_url: z.string().optional().nullable(),
+  status: z.union([z.literal("DEVELOPMENT"), z.literal("PAUSED"), z.literal("RELEASED"), z.literal("LIVE"), z.literal("FINISHED"), z.literal("ABANDONED"), z.literal("STOPPED")]),
+  deleted: z.boolean().optional().nullable().default(false),
+  link_url: z.string().optional().nullable(),
+  link_text: z.string().optional().nullable(),
+  visibility: z.union([z.literal("PUBLIC"), z.literal("PRIVATE"), z.literal("HIDDEN"), z.literal("ARCHIVED"), z.literal("DRAFT"), z.literal("DELETED")]),
+  current_version: z.string().optional()
 });
 
 
@@ -25,41 +25,41 @@ export type UpsertProject = z.infer<typeof upsert_project>;
 
 
 export const upsert_todo = z.object({
-	id: z.string().optional().nullable(),
-	title: z.string(),
-	summary: z.string().optional().nullable(),
-	description: z.string().optional().nullable(),
-	progress: z.union([z.literal("UNSTARTED"), z.literal("IN_PROGRESS"), z.literal("COMPLETED")]),
-	visibility: z.union([z.literal("PUBLIC"), z.literal("PRIVATE"), z.literal("HIDDEN"), z.literal("ARCHIVED"), z.literal("DRAFT"), z.literal("DELETED")]),
-	start_time: z.string().optional().nullable(),
-	end_time: z.string().optional().nullable(),
-	priority: z.union([z.literal("LOW"), z.literal("MEDIUM"), z.literal("HIGH")]),
-	owner_id: z.string(),
-	project_id: z.string().optional().nullable(),
+  id: z.string().optional().nullable(),
+  title: z.string(),
+  summary: z.string().optional().nullable(),
+  description: z.string().optional().nullable(),
+  progress: z.union([z.literal("UNSTARTED"), z.literal("IN_PROGRESS"), z.literal("COMPLETED")]),
+  visibility: z.union([z.literal("PUBLIC"), z.literal("PRIVATE"), z.literal("HIDDEN"), z.literal("ARCHIVED"), z.literal("DRAFT"), z.literal("DELETED")]),
+  start_time: z.string().optional().nullable(),
+  end_time: z.string().optional().nullable(),
+  priority: z.union([z.literal("LOW"), z.literal("MEDIUM"), z.literal("HIGH")]),
+  owner_id: z.string(),
+  project_id: z.string().optional().nullable(),
 });
 
 export type UpsertTodo = z.infer<typeof upsert_todo>;
 
 
 export type UpdateData = {
-	id: string;
-	tag: "todo" | "bug" | "note" | "error";
-	type: "SAME" | "UPDATE" | "DELETE" | "NEW" | "MOVE";
-	data: {
-		old: {
-			text: string,
-			line: number,
-			file: string,
-			context?: string[]
-		},
-		new: {
-			text: string,
-			line: number,
-			file: string,
-			context?: string[]
-		}
-	};
-	task?: Task;
+  id: string;
+  tag: "todo" | "bug" | "note" | "error";
+  type: "SAME" | "UPDATE" | "DELETE" | "NEW" | "MOVE";
+  data: {
+    old: {
+      text: string,
+      line: number,
+      file: string,
+      context?: string[]
+    },
+    new: {
+      text: string,
+      line: number,
+      file: string,
+      context?: string[]
+    }
+  };
+  task?: Task;
 }
 
 export type TodoUpdate = typeof todo_updates.$inferSelect;
@@ -84,3 +84,15 @@ export const upsert_tag = z.object({
 });
 
 export type UpsertTag = z.infer<typeof upsert_tag>;
+
+export const update_user = z.object({
+  id: z.string(),
+  name: z.string().optional(),
+  image_url: z.string().optional(),
+  task_view: z.union([z.literal("list"), z.literal("grid")]).optional(),
+  email_verified: z.boolean().optional(),
+});
+
+export type UpdateUser = z.infer<typeof update_user>;
+
+export type TaskView = NonNullable<UpdateUser['task_view']>;
