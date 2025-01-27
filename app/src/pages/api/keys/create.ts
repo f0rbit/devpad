b9cecc4a9bbecec7764923a043d62b852bfefed6
f0rbit <dev@forbit.dev>
2025-01-27 15:09:50 +1030

    feat: add error handling to generate key

--- app/src/pages/api/keys/create.ts
+++ app/src/pages/api/keys/create.ts
@@ -9,14 +9,18 @@ function getKey(size = 32, format: BufferEncoding = 'hex') {
 
 // we want a POST request handler to generate a random key
 export async function POST(context: APIContext) {
-  if (!context.locals.user) {
-    return new Response(null, { status: 401 });
+  try {
+    if (!context.locals.user) {
+      return new Response(null, { status: 401 });
+    }
+
+    const key = getKey();
+    const new_key = await db.insert(api_key).values({ owner_id: context.locals.user.id, hash: key }).returning();
+    if (new_key.length != 1) throw new Error(`API key generation returned incorrect rows (${new_key.length}`);
+
+    return new Response(JSON.stringify(new_key[0]));
+  } catch (e) {
+    console.error(e);
+    return new Response(null, { status: 500 });
   }
-
-  const key = getKey();
-  const new_key = await db.insert(api_key).values({ owner_id: context.locals.user.id, hash: key }).returning();
-  if (new_key.length != 1) throw new Error(`API key generation returned incorrect rows (${new_key.length}`);
-
-  return new Response(JSON.stringify(new_key[0]));
 }
-
