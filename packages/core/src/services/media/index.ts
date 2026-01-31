970399148bedad92385fdb09cd0b3281a6ab75d5
f0rbit <dev@forbit.dev>
2026-01-31 11:01:15 +1000

    refactor(core): add media barrel exports and package.json updates

---
+++ packages/core/src/services/media/index.ts
@@ -0,0 +1,23 @@
+// Media services barrel export
+
+export { type AccountOwnership, type OwnershipError, type ProfileOwnership, requireAccountOwnership, requireProfileOwnership } from "./auth-ownership";
+export { type Bindings, createContextFromBindings } from "./bindings";
+export { type DeleteConnectionError, type DeleteConnectionResult, deleteConnection } from "./connection-delete";
+export type { AppContext } from "./context";
+export { type CronResult, handleCron, type ProviderFactory as CronProviderFactory } from "./cron";
+export { createDb, type Database } from "./db";
+export { mergeByKey } from "./merge";
+export { createProviderFactory, defaultProviderFactory } from "./platforms";
+export type { ProviderFactory } from "./platforms/types";
+export {
+	initialState,
+	isCircuitOpen,
+	type RateLimitState,
+	shouldFetch,
+	updateOnFailure,
+	updateOnSuccess,
+} from "./rate-limits";
+export * from "./services";
+export { parseStoreId, type StoreId, store } from "./storage";
+export { token as mediaToken } from "./token";
+export { secrets } from "./utils";
